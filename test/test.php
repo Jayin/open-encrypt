1be76cce09c701ef620753969b04d742d4f3078e
Jayin Ton <tonjayin@gmail.com>
2016-08-23 09:51:49 +0800

    More test

--- test/test.php
+++ test/test.php
@@ -11,3 +11,16 @@ $salt = 'China';
 
 echo check_sign($kv, $salt, '26e1779686174c502715a03f4681c4f4');
 
+
+
+//期望值： fca6a86649078bffafd68f6672dbab6f
+echo "\r\n";
+
+$kv = [
+    'appid' => '111',
+    'name' => 'XXX'
+];
+$salt = 'ZZZ';
+
+echo check_sign($kv, $salt, 'fca6a86649078bffafd68f6672dbab6f');
+
